810bdb5f36924a416aed8f886b3b50fd36e19de8
Alexander Schepelmann <aschepel@alumni.cmu.edu>
2018-10-05 16:41:34 -0400

    Code for random photo on load

---
+++ randomPhoto.js
@@ -0,0 +1,9 @@
+window.onload = randPhoto;
+
+//var photos = new Array("images/alex.jpg","images/alex2.jpg","images/alex3.jpg");
+var photos = new Array("images/alex2.jpg","images/alex3.jpg");
+	
+function randPhoto() {
+		var randNum = Math.floor((Math.random() * photos.length));
+		document.getElementById("photoString").src=photos[randNum];
+}
