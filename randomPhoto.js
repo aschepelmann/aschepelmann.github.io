633c67cc2313c1a71d7f5f84e1ca66ba6f39ef1e
Alexander Schepelmann <aschepel@alumni.cmu.edu>
2018-10-28 18:31:18 -0400

    Code edit for line photo

--- randomPhoto.js
+++ randomPhoto.js
@@ -1,7 +1,7 @@
 window.onload = randPhoto;
 
 //var photos = new Array("images/alex.jpg","images/alex2.jpg","images/alex3.jpg");
-var photos = new Array("images/alex2.jpg","images/alex3.jpg","images/alex4.jpg");
+var photos = new Array("images/alex2.jpg","images/alex3.jpg","images/alex4.jpg","images/alexLine.jpg");
 	
 function randPhoto() {
 		var randNum = Math.floor((Math.random() * photos.length));
