30ff641b2d0f2c96e2daec9c1fb6ae87c0695621
Alexander Schepelmann <aschepel@alumni.cmu.edu>
2019-08-22 22:06:25 -0400

    Updated photo loading script

--- randomPhoto.js
+++ randomPhoto.js
@@ -7,7 +7,8 @@ window.onload = randPhoto;
 //var photos = new Array("images/alex2.jpg","images/alex4.jpg","images/alex5.jpg","images/alexLine.jpg");
 //var photos = new Array("images/alex2.jpg","images/alex6.jpg","images/alexLine.jpg");
 //var photos = new Array("images/alex2.jpg","images/alex7.jpg","images/alexLine.jpg");
-var photos = new Array("images/alex2.jpg","images/alex8.jpg","images/alexLine.jpg");
+//var photos = new Array("images/alex2.jpg","images/alex8.jpg","images/alexLine.jpg");
+var photos = new Array("images/alex8.jpg","images/alex9.jpg","images/alexLine.jpg");
 
 function randPhoto() {
 		var randNum = Math.floor((Math.random() * photos.length));
