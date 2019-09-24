4f7dc21259643483476c26281b202d316f5f1d62
Alexander Schepelmann <aschepel@alumni.cmu.edu>
2019-09-24 10:41:30 -0400

    Updated photo

--- randomPhoto.js
+++ randomPhoto.js
@@ -9,6 +9,7 @@ window.onload = randPhoto;
 //var photos = new Array("images/alex2.jpg","images/alex7.jpg","images/alexLine.jpg");
 //var photos = new Array("images/alex2.jpg","images/alex8.jpg","images/alexLine.jpg");
 var photos = new Array("images/alex8.jpg","images/alex9.jpg","images/alexLine.jpg");
+var photos = new Array("images/alex8.jpg","images/alex9.jpg","images/alex10.jpg","images/alexLine.jpg");
 
 function randPhoto() {
 		var randNum = Math.floor((Math.random() * photos.length));
